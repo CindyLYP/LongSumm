93b0e801de4320f1d2bea9bfd78bae1adb7bfded
CindyLYP <scying@zju.edu.cn>
2021-02-21 17:28:14 +0800

    02211648

--- utils/format_test.py
+++ utils/format_test.py
@@ -5,8 +5,11 @@ import json
 with open('../output/test/pred.json', 'r') as f:
     d = json.load(f)
 
+res = {}
 with open("../output/test/test.json",'w') as f:
     for it in d:
         summ = drop_sent(it['pred'])
-        s = json.dumps({it['id']: summ})
-        f.write(s+'\n')
+        res[it['id']] = summ
+    f.write(json.dumps(res))
+
+
